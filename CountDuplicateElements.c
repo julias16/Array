42eba97bf669393b78698772737ccda1f7781216
Julias Uddin Khan <135951641+julias16@users.noreply.github.com>
2023-09-01 16:03:20 +0600

    Update CountDuplicateElements.c

--- CountDuplicateElements.c
+++ CountDuplicateElements.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int arr[100], i, j, n, count=1;
+    int arr[100], i, j, n, count=0;
     printf("How many numbers: ");
     scanf("%d", &n);
     for (i= 0; i< n; i++)
